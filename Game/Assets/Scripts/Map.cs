1a4c36d9b9433b393d1b37bd2d4ad90cf7af75a2
KMCCAL200 <kerrimccallum@outlook.com>
2014-03-07 00:11:26 +0000

    V1

---
+++ Game/Assets/Scripts/Map.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+	Vector3 old;
+	bool h;
+	public MapMove MapMove;
+
+	GameObject target;
+	public float time = 5;
+	Vector3 position;
+	// Use this for initialization
+	void Start () {
+		old = GameObject.Find("BigMap").transform.position;
+		target = gameObject;
+		position = GameObject.Find("BigMapTarget").transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void g(bool j){j=h;}
+
+	void OnMouseDown()
+	{
+		if(gameObject.name == "Map")
+		{	h=true;
+			//MapMove.move(h);
+			Debug.Log("for");
+			//MapMove.Forward(target);
+		}
+//
+//		
+		if(gameObject.name == "BigMap")
+		{	
+			h=false;
+			Debug.Log("back");
+			//MapMove.move(h);
+
+			//MapMove.Back(target);
+		}
+	}
+}
